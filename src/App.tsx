7c7176053cde2cfe87b6b29827bc6f6147243ab0
Noura <nouramursal@gmail.com>
2026-07-24 15:29:02 +0400

    feat : add the HomePage route.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import HomePage from "./pages/HomePage"
 import {Route , createBrowserRouter, createRoutesFromElements,RouterProvider} from 'react-router-dom';
 import InterviewSetup from "./pages/InterviewSetup";
 import Interview from "./pages/Interview";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
 
     const router = createBrowserRouter(createRoutesFromElements(
-      <Route>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/interview-setup" element={<InterviewSetup />}/>
-        <Route path="/interview" element={<Interview />}/>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />}/>
+        {/* <Route path="/interview-setup" element={<InterviewSetup />}/>
+        <Route path="/interview" element={<Interview />}/> */}
 
 
       </Route>
