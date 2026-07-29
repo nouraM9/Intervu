36c87191bae7e4db49f01f55312465c6de67df82
Noura <nouramursal@gmail.com>
2026-07-29 15:21:05 +0400

    feat : add the reusable button component to the home page.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,44 +1,28 @@
-import { useState } from "react"
-import Card from "../components/Card"
-import { Link } from "react-router";
+import { useState } from "react";
+import Card from "../components/Card";
+import Button from "../components/Button";
 
 function HomePage() {
-    const [name, setName] = useState<string>('Noura');
-    const [resumeStatus , setResumeStatus] = useState<string>('Not Uploaded');
-    const [latestScore,setLatestScore] = useState<number>(0); 
-    const [totalInterviewsCount , setTotalInterviewCount] = useState<number>(0);
+  const [name, setName] = useState<string>("Noura");
+  const [resumeStatus, setResumeStatus] = useState<string>("Not Uploaded");
+  const [latestScore, setLatestScore] = useState<number>(0);
+  const [totalInterviewsCount, setTotalInterviewCount] = useState<number>(0);
   return (
-        <div className="mx-auto max-w-7xl px-6 py-16">
-            <div className="mb-10">
-                <h1 className=" title">Welcome , {name}</h1>
-                <p className="body-text-1 mt-2">You have 3 interviews completed,Keep going!</p>
-            </div>
-            <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 mb-[24px]">
-                    <Card title = {'Resume'} value = {resumeStatus} />
-                    <Card title = {'Score'} value = {latestScore} />
-                    <Card title = {'Count'} value = {totalInterviewsCount} />
-                
-
-            </div>
-<Link
-  to="/interview-setup"
-  className="
-    inline-block
-    rounded-xl
-    bg-(--btn)
-    px-30
-    py-4
-    text-(--bg)
-    transition-colors
-    hover:bg-(--btn-gradiant)
-  "
->
-  Start New Interview
-</Link>
-
-        </div>
-  
-  )
+    <div className="mx-auto max-w-7xl px-6 py-16">
+      <div className="mb-10">
+        <h1 className=" title">Welcome , {name}</h1>
+        <p className="body-text-1 mt-2">
+          You have 3 interviews completed,Keep going!
+        </p>
+      </div>
+      <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 mb-[24px]">
+        <Card title={"Resume"} value={resumeStatus} />
+        <Card title={"Score"} value={latestScore} />
+        <Card title={"Count"} value={totalInterviewsCount} />
+      </div>
+      <Button to="/interview-setup">Start New Interview</Button>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
