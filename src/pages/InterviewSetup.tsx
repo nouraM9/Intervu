443b4948083bcec3c3fb242b4790034f7840a9fc
Noura <nouramursal@gmail.com>
2026-07-29 15:18:44 +0400

    feat : add intreview setp page for setting the the intreview state.

--- src/pages/InterviewSetup.tsx
+++ src/pages/InterviewSetup.tsx
@@ -1,61 +1,53 @@
-import { useState } from "react"
-import { Link } from "react-router";
+import { useState } from "react";
+import Button from "../components/Button";
+import Dropdown from "../components/Dropdown";
 
 function InterviewSetup() {
-  const [type , setType] = useState<string>('');
-  const [difficulty , setDifficulty] = useState<string>('')
+  const [type, setType] = useState<string>("select type");
+  const [difficulty, setDifficulty] = useState<string>("select difficulty");
+  const [duration, setDuration] = useState<string>("select duration");
 
-const  handleType = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setType(e.target.value);
-    console.log(type)
-  }
-  const handleDifficulty = (e: React.ChangeEvent<HTMLSelectElement>)=> {
-    setDifficulty(e.target.value);
-    console.log(difficulty)
-  }
+  const interviewTypes = [
+    "front-end",
+    "back-end",
+    "behavioral",
+    "system-design",
+  ];
+  const difficulties = ["Junior", "mid-level", "senior"];
+  const durations = ["15", "20", "30"];
+
   return (
-    <>
-      <h1 className="block">Set Up Your Mock Interview</h1>
-    <div className=" flex flex-col justify-center items-center h-screen " >
-      <div >
-      <label htmlFor="label">Interview Type</label>
-      <select 
-      defaultValue='select' 
-      className="bg-gray-300 rounded-2xl ml-2 select appearance-none px-2"  
-      onChange={handleType}>
-        <option value="select" disabled={true}>Select</option>
-        <option value="FrontEnd">FrontEnd</option>
-        <option value="BackEnd">BackEnd</option>
-        <option value="System Desgin">System Design</option>
-        <option value="Behavioral">Behavioral</option>
-
-      </select>
-
+    <div className=" mx-auto max-w-7xl px-8 py-16 ">
+      <div className="message">
+        <h1 className="title">Set Up Your Mock Interview</h1>
       </div>
-
-    <div>
-
-         <label htmlFor="label">Difficulty</label>
-      <select
-       defaultValue='select' 
-       className="bg-gray-300 rounded-2xl ml-2 select appearance-none px-2"
-       onChange={handleDifficulty}
-        >
-        <option value="select" disabled={true}>Select</option>
-        <option value="Junior">Junior</option>
-        <option value="Med">Med</option>
-        <option value="Senior">Senior </option>
-
-
-      </select>
-    </div>
-      <div className="btn mt-3">
-        <Link to = '/interview' className="bg-blue-600 text-white p-2 rounded d-block">Start Interview </Link>
+      <div className="flex justify-center items-center min-h-screen ">
+        <div className="w-full max-w-xl rounded-xl  bg-white p-8 shadow-(--card-shadow) border border-(--border) p-16">
+          <div className="flex justify-center flex-col">
+            <Dropdown
+              label="Interview Type"
+              options={interviewTypes}
+              value={type}
+              onChange={setType}
+            />
+            <Dropdown
+              label="Difficulty"
+              options={difficulties}
+              value={difficulty}
+              onChange={setDifficulty}
+            />
+            <Dropdown
+              label="Duration"
+              options={durations}
+              value={duration}
+              onChange={setDuration}
+            />
+          </div>
+          <Button to="/interview">Start Interview</Button>
+        </div>
       </div>
     </div>
-    
-    </>
-  )
+  );
 }
 
-export default InterviewSetup
+export default InterviewSetup;
