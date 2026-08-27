719675052994351015b10f854503b5ef830eb27b
Noura <nouramursal@gmail.com>
2026-08-27 21:44:42 +0400

    feat : add the upload resume button.

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -4,7 +4,18 @@ import Button from "../components/Button";
 import axios from "axios";
 import useLocalStorageState from "use-local-storage-state";
 import { useNavigate } from "react-router-dom";
-
+const fileUploadClass = `rounded-xl
+            bg-(--btn)
+            py-4 
+            px-20
+            text-(--bg)
+            transition-colors
+            hover:bg-(--btn-gradiant)
+            absolute
+            left-
+            w-20
+       
+            `;
 function HomePage() {
   const [name, setName] = useState<string>("");
   const [resumeStatus, setResumeStatus] = useState<string>("Not Uploaded");
@@ -13,7 +24,13 @@ function HomePage() {
   const [token] = useLocalStorageState("authToken", { defaultValue: "" });
   const [id] = useLocalStorageState("id", { defaultValue: 0 });
   const navigate = useNavigate();
+  const [fileName, setFileName] = useState<string | null>(null);
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    console.log(e.target.files);
+    setFileName(file ? file.name : null);
+  };
   useEffect(() => {
     if (!token) {
       navigate("/");
@@ -27,8 +44,33 @@ function HomePage() {
       .then((response) => setName(response.data.user.firstName));
   }, [name]);
   return (
-    <div className="mx-auto max-w-7xl px-6 py-16">
-      <div className="mb-10">
+    <div className="mx-auto max-w-7xl px-6 py-16 relative">
+      <label
+        htmlFor="file-upload"
+        className="
+          rounded-xl
+          bg-(--btn)
+          py-4
+          px-20
+          text-(--bg)
+          transition-colors
+          hover:bg-(--btn-gradiant)
+          cursor-pointer
+          inline-block
+          right-0
+          absolute
+        "
+      >
+        {fileName ? fileName : "Choose File"}
+      </label>
+      <input
+        id="file-upload"
+        type="file"
+        onChange={handleFileChange}
+        className="hidden"
+      />
+
+      <div className="mb-10 ">
         <h1 className=" title">Welcome , {name}</h1>
         <p className="body-text-1 mt-2">
           You have 3 interviews completed,Keep going!
