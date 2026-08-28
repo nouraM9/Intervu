726229f807f6ab43c68ea18076e0b6f26d75dfd8
Noura <nouramursal@gmail.com>
2026-08-28 15:29:24 +0400

    add route for resume upload

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/sign-up',[AuthController::class,'register']);
@@ -11,5 +11,7 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/user/{id}',[UserController::class,'me']);
     Route::get('/users',[UserController::class,'ListUsers']);
+    Route::get('/upload-resume',[ResumeController::class,'uploadResume']);
+
 
 });
