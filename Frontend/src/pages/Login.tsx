5f1abead59814cdf3e42643d13c990f39079f60b
noura <nouramursal@gmail.com>
2026-09-19 15:36:00 +0400

    feat : add toast success and error.

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -4,6 +4,8 @@ import logo from "../assets/logo.png";
 import { useState } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 import React from "react";
 function Login() {
@@ -14,6 +16,7 @@ function Login() {
   const [token, setToken] = useLocalStorageState<string>("authToken", {
     defaultValue: "",
   });
+  const [error, setError] = useState<string>("");
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     axios
@@ -24,17 +27,29 @@ function Login() {
       .then((response) => {
         const receivedToken = response.data.token;
         const receivedId = response.data.user.id;
+
         setId(receivedId);
         setToken(receivedToken);
-        console.log("Login successful , Token received");
+        toast.success("Login successful");
         navigate("/dashboard");
       })
-      .catch((error) => console.log(error.response.data.errors));
+      .catch((error) => {
+        if (error.response?.status === 401) {
+          setError("Wrong email or password.");
+        } else {
+          setError("Something went wrong. Please try again.");
+        }
+      });
   };
   return (
     <div className="max-w-7xl mx-auto px-8 py-16">
       <div className="flex min-h-screen justify-center items-center">
         <div className="w-full max-w-md border border-(--border) rounded-2xl shadow-(--card-shadow) bg-white px-8 py-16">
+          {error && (
+            <span className="text-red-500 text-sm flex justify-center ">
+              {error}
+            </span>
+          )}
           <div className="flex justify-center gap-4 text-center items-center heading-1  mb-4">
             <img src={logo} alt="Intervu logo" className="w-13 h-11.5" />
             <span>Intervu</span>
@@ -44,10 +59,11 @@ function Login() {
               <label className=" mb-2 block">Email</label>
               <input
                 type="email"
-                className="w-full rounded-xl border border-(--border) px-4 py-3"
+                className={`w-full px-4 py-3 rounded-xl ${error ? " border border-red-700" : "border border-(--border)"}`}
                 placeholder="example@gmail.com"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                onClick={() => setError("")}
               />
             </div>
 
@@ -55,10 +71,13 @@ function Login() {
               <label className="mb-2 block">Password</label>
               <input
                 type="password"
-                className="w-full rounded-xl border border-(--border) px-4 py-3"
+                className={`w-full px-4 py-3 rounded-xl ${error ? "border border-red-700" : "border border-(--border)"}`}
                 placeholder="*******"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                onClick={() => setError("")}
               />
             </div>
             <div className="text-right">
@@ -78,9 +97,12 @@ function Login() {
               <Button type="submit">Login</Button>
             </div>
             <div className="text-center">
-              <span className="text-(--btn) hover:cursor-pointer hover:text-(--btn-gradiant)">
+              <Link
+                to="./sign-up"
+                className="text-(--btn) hover:cursor-pointer hover:text-(--btn-gradiant)"
+              >
                 Sign Up
-              </span>
+              </Link>
             </div>
           </form>
         </div>
