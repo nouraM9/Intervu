e075e5a190e287fc5b6d912b54c70285e3d79487
noura <nouramursal@gmail.com>
2026-09-19 15:17:17 +0400

    feat : add new route for interview sesssion.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ResumeController;
+use App\Http\Controllers\InterviewController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::post('/sign-up',[AuthController::class,'register']);
@@ -11,7 +13,11 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/user/{id}',[UserController::class,'me']);
     Route::get('/users',[UserController::class,'ListUsers']);
-    Route::get('/upload-resume',[ResumeController::class,'uploadResume']);
+    Route::post('/upload-resume',[ResumeController::class,'UploadResume']);
+    Route::post('/update-resume',[ResumeController::class,'UpdateResume']);
+    Route::get('/users/{userId}/resume',[ResumeController::class,'GetResume']);
+    Route::post('/interview-session',[InterviewController::class,'interviewSession']);
+
 
 
 });
